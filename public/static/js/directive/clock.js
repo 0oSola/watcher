5f786c14923723c10f63dee56de7f319f74a741e
solacg <63364452@qq.com>
2016-09-24 17:47:04 +0800

    24

--- public/static/js/directive/clock.js
+++ public/static/js/directive/clock.js
@@ -14,7 +14,34 @@
 				$scope.getDate = function(){
 					var date = new Date();
 					$scope.timer.year = date.toLocaleDateString().replace(/[/]/g,'.');
-					console.log($scope.timer.year);
+					var time = date.toTimeString();
+					$scope.timer.hour = time.substring(0,time.indexOf(":")+1);
+					$scope.timer.minute = time.substring(time.indexOf(":")+1,time.indexOf(":")+3);
+					switch(date.getDay()){
+						case 0:
+							$scope.timer.day = '星期天';
+							break;
+						case 1:
+							$scope.timer.day = '星期一';
+							break;
+						case 2:
+							$scope.timer.day = '星期二';
+							break;
+						case 3:
+							$scope.timer.day = '星期三';
+							break;
+						case 4:
+							$scope.timer.day = '星期四';
+							break;
+						case 5:
+							$scope.timer.day = '星期五';
+							break;
+						case 6:
+							$scope.timer.day = '星期六';
+							break;
+					}
+					
+					//console.log($scope.timer.year);
 				}
 			},
 			link: function (scope, iElement, iAttrs) {
