a7087c05a2c704e58f38e80a7c3c3ccb80bcb97a
solacg <63364452@qq.com>
2016-10-10 17:21:44 +0800

    1

--- app.js
+++ app.js
@@ -39,6 +39,38 @@
 	  var returnJson = {code: 200, msg: {url: 'http://' + req.headers.host + '/' + filename}};
 	  res.json(returnJson);
 	});*/
+	app.get('/getZone',function(req,res){
+		var type = req.body.type;
+		var jsonp = req.body.jsonp;
+		//console.log(jsonp);
+		//var returnJson = {code: 200, data:{"chart_name":["line1","line2"],"y_name":"人数","x_name":["12/01","12/02","12/03","12/04","12/05","12/06","12/07","12/08","12/09","12/10","12/11","12/12","12/13","12/14","12/15","12/16","12/17","12/18","12/19","12/20"],"chart_data":[[1, 3, 4, 3, 3, 5, 4,3, 4, 3, 5, 4, 2, 6,3, 3, 5, 4,3, 4],[1, 3, 4, 3, 3, 5, 4,3, 4, 3, 5, 4, 2, 6,3, 3, 5, 4,3, 4]],"data_list":[["日期","设备","新增账户","新增设备"],["2015-01-01","11","0","21"],["2012-01-01","11","10","21"],["2018-01-01","1","0","2"]],"totalCount":100,"error_code":0,"error_message":""}};
+		//var returnJson = {code: 200, data:{"chart_name":["line1","line2"],"y_name":"人数","x_name":["12/14","12/15","12/16","12/17","12/18","12/19","12/20"],"chart_data":[[{xname:"12/14",y:3}, {xname:"12/15",y:4}, {xname:"12/16",y:3}, {xname:"12/17",y:5},{xname:"12/18",y:4},{xname:"12/19",y:2},{xname:"12/20",y:6}],[{xname:"12/15",y:1}, {xname:"12/15",y:3}, {xname:"12/16",y:4}, {xname:"12/17",y:3},{xname:"12/18",y:3},{xname:"12/19",y:5},{xname:"12/20",y:4}]],"data_list":[["日期","设备","新增账户","新增设备"],["2015-01-01","11","0","21"],["2012-01-01","11","10","21"],["2018-01-01","1","0","2"]],"totalCount":100,"error_code":0,"error_message":""}};
+		var returnJson = {code: 200, data:['z01','z02','z03','z04','z06']};
+		
+		for(j=0;j<returnJson.data.chart_name.length;j++){
+			var arr = [];
+
+			for(i=1;i<=type;i++){
+				if(i==1){
+					var x_name = [];
+					if(i<10){
+						x = "12/"+'0'+i;
+					}
+					returnJson.data.x_name.push(x);
+				}
+				
+				x_name.push(x);
+				var obj = {
+					xname:x,
+					y:Math.ceil(Math.random()*10)
+				}
+				arr.push(obj);
+			}
+			returnJson.data.chart_data.push(arr);
+		}
+		//console.log(returnJson.data.chart_data);
+	    res.jsonp(returnJson);
+	})
 
 	app.post('/getData',multipart(),function(req,res){
 		var type = req.body.type;
@@ -70,6 +102,33 @@
 			returnJson.data.chart_data.push(arr);
 		}
 		//console.log(returnJson.data.chart_data);
+	    res.jsonp(returnJson);
+	})
+	app.get('/GM_Monitor/QueryMonitorInfoController/QuerMiddle.do',multipart(),function(req,res){
+		var type = req.body.type;
+		//var returnJson = {code: 200, data:{"chart_name":["line1","line2"],"y_name":"人数","x_name":["12/01","12/02","12/03","12/04","12/05","12/06","12/07","12/08","12/09","12/10","12/11","12/12","12/13","12/14","12/15","12/16","12/17","12/18","12/19","12/20"],"chart_data":[[1, 3, 4, 3, 3, 5, 4,3, 4, 3, 5, 4, 2, 6,3, 3, 5, 4,3, 4],[1, 3, 4, 3, 3, 5, 4,3, 4, 3, 5, 4, 2, 6,3, 3, 5, 4,3, 4]],"data_list":[["日期","设备","新增账户","新增设备"],["2015-01-01","11","0","21"],["2012-01-01","11","10","21"],["2018-01-01","1","0","2"]],"totalCount":100,"error_code":0,"error_message":""}};
+		//var returnJson = {code: 200, data:{"chart_name":["line1","line2"],"y_name":"人数","x_name":["12/14","12/15","12/16","12/17","12/18","12/19","12/20"],"chart_data":[[{xname:"12/14",y:3}, {xname:"12/15",y:4}, {xname:"12/16",y:3}, {xname:"12/17",y:5},{xname:"12/18",y:4},{xname:"12/19",y:2},{xname:"12/20",y:6}],[{xname:"12/15",y:1}, {xname:"12/15",y:3}, {xname:"12/16",y:4}, {xname:"12/17",y:3},{xname:"12/18",y:3},{xname:"12/19",y:5},{xname:"12/20",y:4}]],"data_list":[["日期","设备","新增账户","新增设备"],["2015-01-01","11","0","21"],["2012-01-01","11","10","21"],["2018-01-01","1","0","2"]],"totalCount":100,"error_code":0,"error_message":""}};
+		var returnJson = {code: 200, data:{"chart_name":["line1"],"y_name":"人数","x_name":[],"chart_data":[],"error_code":0,"error_message":""}};
+		var returnJson = {"type":"1","action_type":"http","setDate":["2016-09-27 11:04:47","2016-09-27 11:05:48","2016-09-27 11:07:35","2016-09-27 11:08:48","2016-09-27 11:09:52","2016-09-27 11:10:52","2016-09-27 11:11:52","2016-09-27 11:12:53","2016-09-27 11:13:53","2016-09-27 11:14:53","2016-09-27 11:15:54","2016-09-27 11:16:54","2016-09-27 11:17:54","2016-09-27 11:18:55","2016-09-27 11:19:55","2016-09-27 11:20:55","2016-09-27 11:21:56","2016-09-27 11:22:56","2016-09-27 11:23:56"],"status":[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],"zone":[" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "],"zoneNme":[" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "],"withTime":[0,0,21,23,31,28,31,4,49,3,16,30,34,15,3,25,28,13,25],"result":"0"}
+		
+		var arr = [];
+
+		for(i=1;i<=type;i++){
+			if(i<10){
+				x = "12/"+'0'+i;
+			}else{
+				x = "12/"+i;
+			}
+			returnJson.data.x_name.push(x);
+
+			var obj = {
+				xname:x,
+				y:Math.ceil(Math.random()*10)
+			}
+			returnJson.data.chart_data.push(obj);
+		}
+		//console.log(returnJson.data.x_name);
+	    //res.json(req.body.callback+'('+returnJson+')');
 	    res.jsonp(returnJson);
 	})
 
--- public/static/js/directive/chartDirective.js
+++ public/static/js/directive/chartDirective.js
@@ -1,10 +1,35 @@
 (function(){
+
 	var app = angular.module('chartComponent', []);
-	app.directive('boxList',function(){
+
+	app.directive('boxList',['$http','$compile',function($http,$compile){
 		return {
 			restrict:'A',
 			scope:{},
 			link:function($scope,$ele,$attrs){
+				//获取区服
+				$http({
+					url:'http://10.20.72.64:8080/GM_Monitor/QueryMonitorInfoController/QuerMiddle.do?action=sqlRole&jsonp=JSON_CALLBACK',
+					method:'JSONP'
+				}).then(function sucessCallback(response){
+					if(response.data.result == 0){
+						var zoneList = response.data.zoneCode;
+						console.log(zoneList);
+						for(i=0;i<zoneList.length;i++){
+							//var tpl = $($('.tpl').html()).addClass('mix')
+							var tpl = $($('.tpl').html()).addClass('mix');
+							tpl.find('.chart-box').attr('id',zoneList[i]);
+							tpl.find('.main-box').attr('data-zone',zoneList[i]);
+							if(i%2==0){
+								tpl.find('.main-box').attr('data-color','rgs');
+							}
+							tpl = $compile(tpl)($scope);
+							$("#watch-list").mixItUp('append',tpl,{ filter: 'all'});
+						}
+					}
+				},function error(){
+
+				})
 				$ele.find('ul').mixItUp({
 				    controls: {
 				    	enable: false
@@ -20,7 +45,7 @@
 				});
 			}
 		}
-	})
+	}])
 
 	app.directive('box',function(){
 		return {
@@ -29,9 +54,7 @@
 			require:'^?boxList',
 			controller:['$scope','$http',function($scope,$http){
 				this.QuerMiddle = function(type){
-					/*console.log(type);*/
 					return $http({
-						///getData1
 						url:'http://10.20.72.64:8080/GM_Monitor/QueryMonitorInfoController/QuerMiddle.do?action='+type+'&jsonp=JSON_CALLBACK',
 						method:'JSONP'
 					})
@@ -39,6 +62,13 @@
 				this.getWatch_type = function(){
 					return $scope.watch_type;
 				},
+				//角色库
+				this.getRoleData = function(type,zoneCode){
+					return $http({
+						url:'http://10.20.72.64:8080/GM_Monitor/QueryMonitorInfoController/QuerMiddle.do?action='+type+'&zoneCode='+zoneCode+'&jsonp=JSON_CALLBACK',
+						method:'JSONP'
+					})
+				}
 				this.builder = {
 					//图表
 					createChart:function(target_id,chartType,date,withTime,status,action_type,type){
@@ -51,7 +81,7 @@
 								$scope.watch_type = '';
 							break;
 							case 3:
-								$scope.watch_type = '';
+								$scope.watch_type = '角色库连接监控';
 							break;
 							case 4:
 								$scope.watch_type = 'ICHARGE监控';
@@ -119,7 +149,7 @@
 		}
 	})
 
-	app.directive('splineBox',['$timeout','$interval',function($timeout,$interval){
+	app.directive('chartBox',['$timeout','$interval',function($timeout,$interval){
 		return{
 			restrict:'A',
 			scope:true,
@@ -135,22 +165,30 @@
 							var data = response.data;
 							data = angular.fromJson(data);
 							
-							var max = data.status[0];
+							var max = data.withTime[0];
 							var max_idx = 0;
-							for(i=0;i<data.status.length;i++){
-								if(max<data.status[i]){
-									max = data.status[i];
+							for(i=0;i<data.withTime.length;i++){
+								if(max<data.withTime[i]){
+									max = data.withTime[i];
 									max_idx = i;
 								}
 							}
+							scope.action_type = response.data.action_type;
 							var max_timer = data.setDate[max_idx];
 							scope.max_timer = '最高延迟时间点:'+max_timer.substring(max_timer.indexOf('-')+1,max_timer.length-3);
 							scope.max = max.toFixed(2);
 
 							var colorChart = 'redgreensplineChart';
-							if(iAttrs.color=='bo'){
+							if(iAttrs.color=='bos'){
 								colorChart = 'blueorangesplineChart';
+							}else if(iAttrs.color=='rgs'){
+								colorChart = 'redgreensplineChart';
+							}else if(iAttrs.color=='gc'){
+								colorChart = 'greencolumnChart';
+							}else if(iAttrs.color=='bc'){
+								colorChart = 'bluecolumnChart';
 							}
+
 							pctrl.builder.createChart(id,colorChart,data.setDate,data.withTime,data.status,data.action_type,data.type);
 							scope.watch_type = pctrl.getWatch_type();
 						}else{
@@ -160,9 +198,7 @@
 							}, 60000);
 						}
 					},function errorCallBack(response){
-						$timeout(function() {
-							getData();
-						}, 60000);
+						iElement.find('.error-msg').text("连接不上服务器,请联系管理员");
 					})
 				}
 				getData();
@@ -173,120 +209,94 @@
 		}
 	}])
 
-	app.directive('columnBox', ['$interval','$timeout',function ($interval,$timeout) {
-		return {
-			restrict: 'A',
+	app.directive('roleBox',['$timeout','$interval',function($timeout,$interval){
+		return{
+			restrict:'A',
 			scope:true,
 			require:'?^box',
-			link: function (scope, iElement, iAttrs,pctrl) {
-				
+			link:function(scope, iElement, iAttrs,pctrl){
+
 				function getData(){
 					var action_type = iAttrs.action;
-					pctrl.QuerMiddle(action_type).then(function callback(response){
-						if(response.data.result==0){
-							var id = iElement.find('.chart-box').attr('id');
-							iElement.find('.loading').css('display','none');
-
-							var data = response.data;
-							scope.action_type = '类型：'+data.action_type;
-							
-							var max = data.withTime[0];
-							var max_idx = 0;
-							for(i=0;i<data.withTime.length;i++){
-								if(max<data.withTime[i]){
-									max = data.withTime[i];
-									max_idx = i;
+					var zoneCode = iAttrs.zone;
+					if(zoneCode){
+						pctrl.getRoleData(action_type,zoneCode).then(function successCallback(response){
+							if(response.data.result==0){
+								var id = iElement.find('.chart-box').attr('id');
+								iElement.find('.loading').css('display','none');
+								var data = response.data;
+								data = angular.fromJson(data);
+								
+								var max = data.withTime[0];
+								var max_idx = 0;
+								for(i=0;i<data.withTime.length;i++){
+									if(max<data.withTime[i]){
+										max = data.withTime[i];
+										max_idx = i;
+									}
 								}
+								scope.action_type = response.data.action_type;
+								scope.zone_name = '区服名：'+response.data.zoneNme[0];
+								var max_timer = data.setDate[max_idx];
+								scope.max_timer = '最高延迟时间点:'+max_timer.substring(max_timer.indexOf('-')+1,max_timer.length-3);
+								scope.max = max.toFixed(2);
+
+								var colorChart = 'redgreensplineChart';
+								if(iAttrs.color=='bos'){
+									colorChart = 'blueorangesplineChart';
+								}else if(iAttrs.color=='rgs'){
+									colorChart = 'redgreensplineChart';
+								}else if(iAttrs.color=='gc'){
+									colorChart = 'greencolumnChart';
+								}else if(iAttrs.color=='bc'){
+									colorChart = 'bluecolumnChart';
+								}
+
+								pctrl.builder.createChart(id,colorChart,data.setDate,data.withTime,data.status,data.action_type,data.type);
+								scope.watch_type = pctrl.getWatch_type();
+							}else{
+								iElement.find('.error-msg').text(response.data.message);
+								$timeout(function() {
+									getData();
+								}, 60000);
 							}
-							var max_timer = data.setDate[max_idx];
-							scope.max_timer = '最高延迟时间点:'+max_timer.substring(max_timer.indexOf('-')+1,max_timer.length-3);
-							scope.max = max.toFixed(2);
-							pctrl.builder.createChart(id,'bluecolumnChart',data.setDate,data.withTime,data.status,data.action_type,data.type);
-							scope.watch_type = pctrl.getWatch_type();
-						}else{
-							iElement.find('.error-msg').text(response.data.message);
-							$timeout(function() {
-								getData();
-							}, 60000);
-						}
-					},function errorCallBack(response){
-						iElement.find('.error-msg').text("连接不上服务器,请联系管理员");
-					})
+						},function errorCallBack(response){
+							iElement.find('.error-msg').text("连接不上服务器,请联系管理员");
+						})
+					}
 				}
 				getData();
 				$interval(function() {
-				   getData();
+					getData();
 				}, 1200000);
-				
 			}
-		};
+		}
 	}])
-	app.directive('columnSingleBox', ['$interval','$timeout',function ($interval,$timeout) {
+
+	app.directive('pieBox', ['$interval','$timeout',function ($interval,$timeout) {
 		return {
 			restrict: 'A',
 			require:'?^box',
 			scope:true,
 			link: function (scope, iElement, iAttrs,pctrl) {
+
 				function getData(){
-					var action_type = iAttrs.action;
-					pctrl.QuerMiddle(action_type).then(function successCallback(response){
+					var id = iElement.find('.chart-box').attr('id');
+					iElement.find('.loading').css('display','none');
+					pctrl.request().then(function successCallback(response){
 						if(response.data.result==0){
-							var id = iElement.find('.chart-box').attr('id');
-							iElement.find('.loading').css('display','none');
-
-							var data = response.data;
-							scope.action_type = '类型：'+data.action_type;
-
-							var max = data.withTime[0];
-							var max_idx = 0;
-							for(i=0;i<data.withTime.length;i++){
-								if(max<data.withTime[i]){
-									max = data.withTime[i];
-									max_idx = i;
-								}
-							}
-							var max_timer = data.setDate[max_idx];
-							scope.max_timer = '最高延迟时间点：'+max_timer.substring(max_timer.indexOf('-')+1,max_timer.length-3);
-							scope.max = max.toFixed(2);
-							
-							var data = response.data;
-							data = angular.fromJson(data);
-							pctrl.builder.createChart(id,'greencolumnChart',data.setDate,data.withTime,data.status,data.action_type,data.type)
-							scope.watch_type = pctrl.getWatch_type();
-						}else{
-							iElement.find('.error-msg').text(response.data.message);
-							$timeout(function(){
-								getData();
-							},60000);
+							var data = angular.fromJson(response.data.data);
+							pctrl.builder.buildChart(id,'pieChart',data.setDate,data.withTime,data.status,data.action_type,data.type);
 						}
-					},function errorcallback(response){
+					},function errorCallback(){
 						iElement.find('.error-msg').text("连接不上服务器,请联系管理员");
 					})
 				}
+
 				getData();
 				$interval(function(){
 					getData();
 				},1200000)
-			}
-		};
-	}])
-
-	app.directive('pieBox', ['$timeout',function ($timeout) {
-		return {
-			restrict: 'A',
-			require:'?^box',
-			scope:true,
-			link: function (scope, iElement, iAttrs,pctrl) {
-				$timeout(function(){
-					var id = iElement.find('.chart-box').attr('id');
-					iElement.find('.loading').css('display','none');
-					pctrl.request().then(function successCallback(response){
-						var data = angular.fromJson(response.data.data);
-						pctrl.builder.buildChart(id,'pieChart',data.chart_data,data.x_name,data.y_name,data.chart_name);
-					},function errorCallback(){
-
-					})
-				},1000)
 			}
 		};
 	}])
